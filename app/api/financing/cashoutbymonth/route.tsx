6c7162d3f9681c94c8dc95877d43902af48357c0
Sercan Atalik <sercan@atalik.com>
2025-06-21 10:05:53 +0100

    api

--- app/api/financing/cashoutbymonth/route.tsx
+++ app/api/financing/cashoutbymonth/route.tsx
@@ -5,26 +5,28 @@ import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
   try {
-    const { filter = null } = await req.json()
-
+    const { filter = null,breakdown = 'desk' } = await req.json()
+    let breakdownClause =  breakdown ? `, ${breakdown}` : ''
+    let breakdownPartition = breakdown ? `PARTITION BY ${breakdown}` : ''
     const query = `
       WITH monthly_data AS (
         SELECT
           toStartOfMonth(asOfDate) as month_start,
-          sum(cashOut) as monthly_cashout
+          sum(cashOut) as monthly_cashout${breakdownClause} 
         FROM risk_f_mv
         ${buildWhereCondition(filter,true)}
-        GROUP BY month_start
+        GROUP BY month_start${breakdownClause}
         ORDER BY month_start
       )
       
       SELECT
-        formatDateTime(month_start, '%Y-%m') as month,
-        sum(monthly_cashout) OVER (ORDER BY month_start) as cumulative_cashout,
+        formatDateTime(month_start, '%Y-%m') as month${breakdownClause}, 
+        sum(monthly_cashout) OVER (${breakdownPartition} ORDER BY month_start) as cumulative_cashout,
         monthly_cashout
       FROM monthly_data
-      ORDER BY month_start
+      ORDER BY month_start${breakdownClause}
     `
+    console.log(query)
     const resultSet = await getClickHouseClient().query({
       query,
       format: "JSONEachRow",
